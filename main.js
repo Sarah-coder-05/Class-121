cb091b8a462b07853671bb4ce34e1e39ee1eb63b
Sarah-coder-05 <76991629+Sarah-coder-05@users.noreply.github.com>
2021-07-22 13:30:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function setup() {
 }
 
 function modelLoaded() {
-  console.log('ModelLoaded!');
+  console.log('Model Loaded!');
 }
 
 function draw() {
@@ -20,6 +20,7 @@ function gotResult(error, results) {
  console.error(error);
  }  else {
    console.log(results);
-   document.getElementById("result_object_name").innerHTMl = results[0].label;
-   document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); }
+   document.getElementById("result_object_name").innerHTML = results[0].label;
+   document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3); 
+  }
  }
